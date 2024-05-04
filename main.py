a2cb411d89a6371bbc76a55a24ed5af038103a53
omatsolaeyewu-edero <omatsolajennings@yahoo.com>
2024-05-04 15:58:21 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#importing packages
 import pypickle as py
 import streamlit as st
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+#creating title and header for the streamlit app
 st.title("Automotive Vehicle Engine Health")
 st.header("By leveraging the measurements obtained from various sensors, technicians can input these data points and infer the condition or health status of an automotive engine")
+#loading the model from collab
 model=py.load("lg.pkl")
-#testing the model using logistic regression because it performed better than other models
+#creating user's input, ENTER button and prediction display for the streamlit app
 user_input1=st.number_input('Engine rpm ')
 user_input2=st.number_input('Lub oil pressure ')
 user_input3=st.number_input('Fuel pressure ')
